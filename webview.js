56ef404f8382c86dc091cfd99aec521e775753c7
austin ce <austin.cawley@gmail.com>
2018-12-07 13:21:30 -0500

    fix: correctly create array from query selector collection

--- webview.js
+++ webview.js
@@ -1,26 +1,22 @@
-import path from 'path';
-
-console.log('symphony recipe loaded');
+const path = require('path');
 
 const SELECTOR_BADGE_COUNT = '#conversations .navigation-item .navigation-badge-count';
 
 function getMessages() {
   const directMessages = document.querySelectorAll(SELECTOR_BADGE_COUNT);
-  console.log('symphony messsages', directMessages);
-  return directMessages.reduce((totalCount, elem) => {
-    const numMessages = Number(elem.innerText);
-    if (Number.isNaN(numMessages)) {
-      return totalCount;
-    }
-    return totalCount + numMessages;
-  }, 0);
+  return Array.from(directMessages)
+    .reduce((totalCount, elem) => {
+      const numMessages = Number(elem.innerText);
+      if (Number.isNaN(numMessages)) {
+        return totalCount;
+      }
+      return totalCount + numMessages;
+    }, 0);
 }
 
 
 module.exports = (Franz) => {
-  console.log('Franz loaded', Franz);
   Franz.loop(() => {
-    console.log('Getting Symphony messages');
     const messages = getMessages();
     Franz.setBadge(messages);
   });
